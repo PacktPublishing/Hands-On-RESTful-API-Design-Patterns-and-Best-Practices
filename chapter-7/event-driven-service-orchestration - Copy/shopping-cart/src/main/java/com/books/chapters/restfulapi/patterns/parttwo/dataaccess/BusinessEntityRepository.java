ffe4a98a231b1463a839b7c05f3507f9605f52ea
jayaramanhari <jayaramanhari@gmail.com>
2019-01-02 22:31:48 +0530

    removed the unnecessary project files

--- chapter-7/event-driven-service-orchestration - Copy/shopping-cart/src/main/java/com/books/chapters/restfulapi/patterns/parttwo/dataaccess/BusinessEntityRepository.java
+++
@@ -1,6 +0,0 @@
-package com.books.chapters.restfulapi.patterns.parttwo.dataaccess;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface BusinessEntityRepository extends CrudRepository<BusinessEntityJpa, String> {
-}
