a73381308c3a4a60ef22b985343274918a20de98
Harihara Subramanian <jayaramanhari@gmail.com>
2019-01-05 10:42:30 +0530

    Update runMyExamples.bat

--- scripts/runMyExamples.bat
+++ scripts/runMyExamples.bat
@@ -1,1 +1,1 @@
-java -jar bookexamples/ch03/Hands-On-RESTful-API-Design-Patterns-and-Best-Practices/investor-services/target/investor-services-0.0.1-SNAPSHOT.jar
+java -jar Hands-On-RESTful-API-Design-Patterns-and-Best-Practices/chapter-3/investor-services/target/investor-services-0.0.1-SNAPSHOT.jar
