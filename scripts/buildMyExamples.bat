44d8d3792686a1aaf6542f055ecefb7e13402e17
Harihara Subramanian <jayaramanhari@gmail.com>
2019-01-05 10:27:43 +0530

    Update buildMyExamples.bat

--- scripts/buildMyExamples.bat
+++ scripts/buildMyExamples.bat
@@ -1,7 +1,3 @@
-mkdir bookexamples
-cd bookexamples
-mkdir ch03
-cd ch03
 git clone https://github.com/PacktPublishing/Hands-On-RESTful-API-Design-Patterns-and-Best-Practices.git
 cd Hands-On-RESTful-API-Design-Patterns-and-Best-Practices
 cd investor-services
